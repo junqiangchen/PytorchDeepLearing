cb6c88116ccb3a2b80e9bc0c8b116ee89278e571
junqiangchen <1207173174@qq.com>
2024-02-17 14:36:44 +0800

    Update VNet3dregistration.py

--- networks/VNet3dregistration.py
+++ networks/VNet3dregistration.py
@@ -3,6 +3,48 @@ import torch.nn as nn
 import voxelmorph as vxm
 
 
+class SpatialTransformergrid(nn.Module):
+    """
+    N-D Spatial Transformer
+    """
+
+    def __init__(self, size):
+        super().__init__()
+
+        # create sampling grid
+        vectors = [torch.arange(0, s) for s in size]
+        grids = torch.meshgrid(vectors)
+        grid = torch.stack(grids)
+        grid = torch.unsqueeze(grid, 0)
+        grid = grid.type(torch.FloatTensor)
+
+        # registering the grid as a buffer cleanly moves it to the GPU, but it also
+        # adds it to the state dict. this is annoying since everything in the state dict
+        # is included when saving weights to disk, so the model files are way bigger
+        # than they need to be. so far, there does not appear to be an elegant solution.
+        # see: https://discuss.pytorch.org/t/how-to-register-buffer-without-polluting-state-dict
+        self.register_buffer('grid', grid)
+
+    def forward(self, flow):
+        # new locations
+        new_locs = self.grid + flow
+        shape = flow.shape[2:]
+
+        # need to normalize grid values to [-1, 1] for resampler
+        for i in range(len(shape)):
+            new_locs[:, i, ...] = 2 * (new_locs[:, i, ...] / (shape[i] - 1) - 0.5)
+
+        # move channels dim to last position
+        # also not sure why, but the channels need to be reversed
+        if len(shape) == 2:
+            new_locs = new_locs.permute(0, 2, 3, 1)
+            new_locs = new_locs[..., [1, 0]]
+        elif len(shape) == 3:
+            new_locs = new_locs.permute(0, 2, 3, 4, 1)
+            new_locs = new_locs[..., [2, 1, 0]]
+        return new_locs
+
+
 class LUConv3d(nn.Module):
     def __init__(self, nchan, prob):
         """
